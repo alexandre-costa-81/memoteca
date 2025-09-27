e3e17c1480eef317f6c97339d38b54e51cc492a4
Alexandre Costa <alexandre.costa@furg.br>
2025-09-27 17:03:44 -0300

    feat: enable two-way data binding for pensamento properties in CriarPensamento component

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './criar-pensamento.html',
   styleUrl: './criar-pensamento.css'
 })
