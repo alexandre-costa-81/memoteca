a8d4429efcc437216753c89a3a318568a9c44f55
Alexandre Costa <alexandre.costa@furg.br>
2025-09-27 17:18:42 -0300

    feat: remove CriarPensamento and ListarPensamento components from app imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Cabecalho } from "./componentes/cabecalho/cabecalho";
 import { Rodape } from "./componentes/rodape/rodape";
-import { CriarPensamento } from "./componentes/pensamentos/criar-pensamento/criar-pensamento";
-import { ListarPensamento } from "./componentes/pensamentos/listar-pensamento/listar-pensamento";
 import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Cabecalho, Rodape, CriarPensamento, ListarPensamento, RouterOutlet],
+  imports: [Cabecalho, Rodape, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
