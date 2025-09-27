abcce978641b87551ba42a25689d422fd6c70d85
Alexandre Costa <alexandre.costa@furg.br>
2025-09-27 14:55:53 -0300

    refactor: remove unused RouterOutlet import from app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Cabecalho } from "./componentes/cabecalho/cabecalho";
 import { Rodape } from "./componentes/rodape/rodape";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Cabecalho, Rodape],
+  imports: [Cabecalho, Rodape],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
