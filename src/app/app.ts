5990ea7a303c206d9b34fd7617ed7807e29f2686
Alexandre Costa <alexandre.costa@furg.br>
2025-09-27 17:10:39 -0300

    feat: add ListarPensamento component with template, styles, and tests

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 import { Cabecalho } from "./componentes/cabecalho/cabecalho";
 import { Rodape } from "./componentes/rodape/rodape";
 import { CriarPensamento } from "./componentes/pensamentos/criar-pensamento/criar-pensamento";
+import { ListarPensamento } from "./componentes/pensamentos/listar-pensamento/listar-pensamento";
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [Cabecalho, Rodape, CriarPensamento],
+  imports: [Cabecalho, Rodape, CriarPensamento, ListarPensamento, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
