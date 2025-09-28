f6ff8c63829b7a8f71cfac6c78b33c1a86f9724d
Alexandre Costa <alexandre.costa@furg.br>
2025-09-28 16:38:12 -0300

    feat: implement 'Carregar Mais' button functionality to load additional thoughts

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.ts
@@ -3,22 +3,33 @@ import { RouterLink } from '@angular/router';
 import { Pensamento } from "../pensamento/pensamento";
 import { PensamentoInterface } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { BotaoCarregarMais } from "../listar-pensamentos/botao-carregar-mais/botao-carregar-mais";
 
 @Component({
   selector: 'app-listar-pensamento',
-  imports: [RouterLink, Pensamento],
+  imports: [RouterLink, Pensamento, BotaoCarregarMais],
   templateUrl: './listar-pensamento.html',
   styleUrl: './listar-pensamento.css'
 })
 export class ListarPensamento {
   listaPensamentos: PensamentoInterface[] = [];
   paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
   constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual).subscribe((pensamentos) => {
       this.listaPensamentos = pensamentos;
+    });
+  }
+
+  carregarMaisPensamentos(): void {
+    this.service.listar(++this.paginaAtual).subscribe((pensamentos) => {
+      this.listaPensamentos.push(...pensamentos);
+      if (!pensamentos.length) {
+        this.haMaisPensamentos = false;
+      }
     });
   }
 }
