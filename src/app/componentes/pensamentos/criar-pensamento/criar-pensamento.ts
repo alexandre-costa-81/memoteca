083880c63b7a28d5f114508f86f90d40c9d4946c
Alexandre Costa <alexandre.costa@furg.br>
2025-09-28 11:26:09 -0300

    feat: add error messages for form validation in CriarPensamento component

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
@@ -25,14 +25,20 @@ export class CriarPensamento {
 
   ngOnInit() {
     this.formulario = this.formBuilder.group({
-      conteudo: ['', [Validators.required]],
-      autoria: ['', [Validators.required]],
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/),
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+      ])],
       modelo: ['modelo1']
     });
   }
 
   criarPensamento() {
-    console.log(this.formulario);
+    console.log(this.formulario.get('autoria')?.errors);
     if (this.formulario.valid) {
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listarPensamento']);
