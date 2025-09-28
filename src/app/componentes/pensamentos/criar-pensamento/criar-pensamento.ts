44f96824bc8f280719545c225100381d58fe36b7
Alexandre Costa <alexandre.costa@furg.br>
2025-09-28 11:36:41 -0300

    feat: enhance button functionality and styling in CriarPensamento component

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { PensamentoService } from '../pensamento.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,6 +10,7 @@ import { PensamentoService } from '../pensamento.service';
     FormsModule,
     RouterLink,
     ReactiveFormsModule,
+    NgClass
 ],
   templateUrl: './criar-pensamento.html',
   styleUrl: './criar-pensamento.css'
@@ -43,6 +45,14 @@ export class CriarPensamento {
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listarPensamento']);
       });
+    }
+  }
+
+  habilitarBotao() {
+    if (this.formulario.valid) {
+      return 'botao';
+    } else {
+      return 'botao__desabilitado';
     }
   }
 }
