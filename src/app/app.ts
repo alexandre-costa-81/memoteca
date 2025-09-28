0635078e767e51c3578e709a67d2aa9eee7f5c24
Alexandre Costa <alexandre.costa@furg.br>
2025-09-28 10:46:04 -0300

    feat: refactor CriarPensamento component to use reactive forms and update template bindings

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,17 @@ import { Component, signal } from '@angular/core';
 import { Cabecalho } from "./componentes/cabecalho/cabecalho";
 import { Rodape } from "./componentes/rodape/rodape";
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Cabecalho, Rodape, RouterOutlet],
+  imports: [
+    Cabecalho,
+    Rodape,
+    RouterOutlet,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.ts
@@ -1,30 +1,38 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { PensamentoService } from '../pensamento.service';
-import { PensamentoInterface } from '../pensamento';
 
 @Component({
   selector: 'app-criar-pensamento',
-  imports: [FormsModule, RouterLink],
+  imports: [
+    FormsModule,
+    RouterLink,
+    ReactiveFormsModule,
+],
   templateUrl: './criar-pensamento.html',
   styleUrl: './criar-pensamento.css'
 })
 export class CriarPensamento {
 
-  pensamento: PensamentoInterface = {
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  };
+  formulario!: FormGroup;
 
   constructor(
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder,
   ) {}
 
+  ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário reativo'],
+      autoria: [''],
+      modelo: ['modelo1']
+    });
+  }
+
   salvarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() => {
+    this.service.criar(this.formulario.value).subscribe(() => {
       this.router.navigate(['/listarPensamento']);
     });
   }
